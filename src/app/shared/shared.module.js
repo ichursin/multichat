4d09f6640781a5e2d461dd91442e56d0bc846d18
Ilya Chursin <chursin.ilya@gmail.com>
2016-11-23 03:50:34 +0300

    some small refactorings
    a) update .gitignore
    b) fix naming in shared.module.js
    c) publish src/public folder

--- src/app/shared/shared.module.js
+++ src/app/shared/shared.module.js
@@ -1,18 +1,18 @@
 import angular from 'angular';
 
-import Peka2tvSocket from './services/handlers/peka-handler.service';
-import GoodgameSocket from './services/handlers/goodgame-handler.service';
+import Peka2tvMessageHandler from './services/handlers/peka-handler.service';
+import GoodgameMessageHandler from './services/handlers/goodgame-handler.service';
 import TwitchMessageHandler from './services/handlers/twitch-handler.service';
+import LiveCodingMessageHandler from "./services/handlers/livecoding-handler.service";
 
 import GoodgameSmiles from './services/goodgame.smiles';
 import Platforms from './services/platforms.service';
 import Messages from './services/messages.service';
-import LiveCodingMessageHandler from "./services/handlers/livecoding-handler.service";
 
 
 export default angular.module('multichat.shared', [])
-    .service('peka2tvMessageHandler', Peka2tvSocket)
-    .service('goodgameMessageHandler', GoodgameSocket)
+    .service('peka2tvMessageHandler', Peka2tvMessageHandler)
+    .service('goodgameMessageHandler', GoodgameMessageHandler)
     .factory('goodgameSmiles', GoodgameSmiles)
     .service('twitchMessageHandler', TwitchMessageHandler)
     .service('livecodingMessageHandler', LiveCodingMessageHandler)
